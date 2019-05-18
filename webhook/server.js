aeab928433abec2a7be4bd195f2b103b25d8bd95
Yu-Jung Ko <yujko@cs.stonybrook.edu>
2019-05-18 13:36:07 -0400

    [ERROR] fail access ShipEngine API with node

--- webhook/server.js
+++ webhook/server.js
@@ -1,12 +1,24 @@
+// Optional: Chatbot
+// + Text Cleaning Process
+
+
+
 'use strict';
 
-// [START gae_node_request_example]
-const express = require('express');
-
+const express = require('express');  // State the basic RESTful API (TOOD: state?)
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const https = require('https');      // Make the request to ShipEngine, FTL API
 const app = express();
 
 
-const rootUrl = '/';
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+const rootUrl = '/api/v1/';
+const validateAddrUrl = rootUrl + 'validateaddress/';
 
 
 // rootUrl: /
@@ -15,6 +27,82 @@ app.get(rootUrl, (req, res) => {
     .status(200)
     .send('Hello, world!')
     .end();
+});
+
+
+
+
+// validateAddrUrl
+app.post(validateAddrUrl, async (req, res) => {
+
+    // TODO: get the api key from the header
+    let se_apikey = process.env.API_KEY;
+    let se_addr = {
+            name: 'Mickey',
+            phone: '714-781-4565',
+            company_name: 'The Walt Disney Company',
+            address_line1: req.body.address,
+            city_locality: 'Burbank',
+            state_province: 'CA',
+            postal_code: '91521',
+            country_code: 'US'
+    };
+
+    try {
+        let response = await axios.post("https://api.shipengine.com/v1/addresses/validate", {
+            headers: {
+                "API-KEY": se_apikey,
+                "Content-Type": "application/json"
+            },
+            data: se_addr
+        });
+
+       console.log(response.data);
+    } catch (e) {
+        console.log(e);
+        console.log(se_apikey);
+    }
+
+    // let options = {
+    //     "method": "POST",
+    //     "hostname": "https://api.shipengine.com/",
+    //     "path": "v1/addresses/validate",
+    //     "headers": {
+    //         "Content-Type": "application/json",
+    //         "api-key": se_apikey
+    //     }
+    // };
+
+
+    // let sereq = https.request(options, function (seres) {
+
+    //     seres.on("data", (data) => {
+    //         console.log("response from se: "+ data);
+    //     });
+
+    //     // res.on("end", function () {
+    //     //     var body = Buffer.concat(chunks);
+    //     //     console.log(body.toString());
+    //     // });
+    // });
+
+    // sereq.write(JSON.stringify([
+    //     {
+    //         name: 'Mickey',
+    //         phone: '714-781-4565',
+    //         company_name: 'The Walt Disney Company',
+    //         address_line1: req.body.address,
+    //         city_locality: 'Burbank',
+    //         state_province: 'CA',
+    //         postal_code: '91521',
+    //         country_code: 'US'
+    //     }
+    // ]));
+    // // req.end();
+    // console.log("apikey: " + apikey);
+    return res.status(200).send({
+        success: 'true'
+    })
 });
 
 // Start the server
