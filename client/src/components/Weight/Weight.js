75fe77e6308212f4f45b4be34b18abae0956f8f7
samuel fagbamigbeE <samuelfagbamigbe@gmail.com>
2019-05-18 23:40:58 -0400

    modified weight js and sign in js

--- client/src/components/Weight/Weight.js
+++ client/src/components/Weight/Weight.js
@@ -1,28 +1,122 @@
 import React, {Component} from 'react';
+// import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-export default function Weight(props){
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  textField: {
+    flexBasis: 200,
+  },
+});
 
-    return(
-      <div>
-      <h1>Weignt</h1>
-      <form onSubmit={props.handleSubmit}>
-        <input type='text'
-        placeholder='value'
-        name='value'
-        value={props.value}
-        onChange={props.handleChange}
+const ranges = [
+  {
+    value: '0-20',
+    label: '0 to 20',
+  },
+  {
+    value: '21-50',
+    label: '21 to 50',
+  },
+  {
+    value: '51-100',
+    label: '51 to 100',
+  },
+];
+
+class Weight extends React.Component {
+  state = {
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  };
+
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword: !state.showPassword }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <TextField
+          id="outlined-simple-start-adornment"
+          className={classNames(classes.margin, classes.textField)}
+          variant="outlined"
+          label="With outlined TextField"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">ounces</InputAdornment>,
+          }}
+        />  
+
+        <TextField
+          id="outlined-adornment-weight"
+          className={classNames(classes.margin, classes.textField)}
+          variant="outlined"
+          label="Weight"
+          value={this.state.weight}
+          onChange={this.handleChange('weight')}
+          helperText="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+          }}
         />
-        <input type='text'
-        placeholder='ounces'
-        name='unit'
-        value={props.unit}
-        onChange={props.handleChange}
-        />
-        <button type='submit'>Calculate prices</button>
-      </form>
 
       </div>
-
-
-    )
+    );
+  }
 }
+
+OutlinedInputAdornments.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Weight);
+
+// export default function Weight(props){
+
+//     return(
+//       <div>
+//       <h1>Weignt</h1>
+//       <form onSubmit={props.handleSubmit}>
+//         <input type='text'
+//         placeholder='value'
+//         name='value'
+//         value={props.value}
+//         onChange={props.handleChange}
+//         />
+//         <input type='text'
+//         placeholder='ounces'
+//         name='unit'
+//         value={props.unit}
+//         onChange={props.handleChange}
+//         />
+//         <button type='submit'>Calculate prices</button>
+//       </form>
+
+//       </div>
+
+
+//     )
+// }
